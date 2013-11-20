00e9f858fd913c38bfda5094414778310f9939b6
Chris Risner <github@chrisrisner.com>
2013-11-19 16:11:21 -0800

    Adding simple bucket raindrop game

--- Echo-desktop/src/com/me/mygdxgame/Main.java
+++ Echo-desktop/src/com/me/mygdxgame/Main.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Echo";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		//cfg.useGL20 = false;
+		cfg.width = 800;
+		cfg.height = 480;
 		
 		new LwjglApplication(new MyGdxGame(), cfg);
 	}
